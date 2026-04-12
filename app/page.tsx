27e3096c8fb7c061e3c3f6e7554bac541333bc4a
venchiheng <chiheng357@gmail.com>
2026-04-13 02:03:22 +0700

    testing middleware v15

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,26 @@ import { cookies } from 'next/headers'
 
 export default async function RootPage() {
   const cookieStore = await cookies()
+
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies: { get(name) { return cookieStore.get(name)?.value } } }
+    {
+      cookies: {
+        get(name) {
+          return cookieStore.get(name)?.value
+        },
+      },
+    }
   )
 
-  const { data: { user } } = await supabase.auth.getUser()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
 
   if (!user) {
     redirect('/login')
-  } else {
-    redirect('/dashboard')
   }
+
+  redirect('/my-dashboard')
 }
