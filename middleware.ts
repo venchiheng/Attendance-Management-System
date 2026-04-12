e6a572c65c3ebad16318b7da1a2863f71cb3cd03
venchiheng <chiheng357@gmail.com>
2026-04-13 00:37:37 +0700

    update middleware v3

--- middleware.ts
+++ middleware.ts
@@ -80,7 +80,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  runtime: "nodejs",
+  // runtime: "nodejs",
   matcher: [
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
