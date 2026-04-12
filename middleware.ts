cb56e41176a894cf5a8cb99c5a346f61128c1282
venchiheng <chiheng357@gmail.com>
2026-04-13 01:13:26 +0700

    testing middleware v7

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
-// proxy.ts
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   let response = NextResponse.next({
     request,
   });
